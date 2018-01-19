3c085ae4bbd8da4148a5a5ad01492d72afe84097
Nate Klein <nak314159@gmail.com>
2018-01-19 11:25:23 -0500

    Added input screen, removed map screen

---
+++ app/src/main/java/adsb/project/awaken/InputScreen.java
@@ -0,0 +1,13 @@
+package adsb.project.awaken;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_screen);
+    }
+}
